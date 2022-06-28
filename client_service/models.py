6d24534d9edad8ef4b8f2943812dc5f65c08b79f
il <sh.il252255@gmail.com>
2022-06-28 13:49:37 +0300

    feat: created models

--- client_service/models.py
+++ client_service/models.py
@@ -1,3 +1,142 @@
 from django.db import models
+import pytz
 
-# Create your models here.
+from languages_list import LANGUAGES
+
+
+class ServiceUser(models.Model):
+    """
+    Базовая информация о пользователях, пока решил не совмещать эту таблицу с пользователями джанго,
+    не уверен, что так правильно, но пока мне так проще учитывая что пока доступ будет только из телеграмм.
+    tg_id: id пользователя в телеграмме;
+    first_name: по-умолчанию буду брать из телеграмм;
+    last_name: дополнительно и необязательно, тоже из телеграмм;
+    tg_username: тоже существует только по тому что могу получить эти данные;
+    language: язык пользователя;
+    timezone: часовой пояс пользователя основано на pytz, для того чтобы не присылать оповещения ночью (добавлю позже);
+    registration_date: дата регистрации она же дата создания записи;
+    role: роль пользователя, тоже для дальнейших доработок;
+    is_delete: при удалении пользователя сохранять его данные;
+    """
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+    ROLES = (('admin', 'admin'), ('moderator', 'moderator'), ('user', 'user'))
+
+    tg_id = models.BigIntegerField(unique=True, blank=True)
+    first_name = models.CharField()
+    last_name = models.CharField(null=True, blank=True)
+    tg_username = models.CharField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(null=True, blank=True)
+    language = models.CharField(max_length=2, choices=LANGUAGES, default='ru')
+    timezone = models.CharField(max_length=32, choices=TIMEZONES, default='UTC')
+    registration_date = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=20, choices=ROLES, default='user')
+    is_delete = models.BooleanField(default=False)
+    password = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name+self.last_name
+
+
+class Words(models.Model):
+    """
+    Все слова, что есть в системе с наиболее популярными (или первыми добавленными) переводами.
+    Пока поля word_class (часть речи), past_form (вторая форма неправильного глагола),
+    past_participle (третья форма неправильного глагола) добавлены как заглушки на будущее.
+    """
+    word = models.CharField(unique=True)
+    def_translations = models.CharField()
+    word_class = models.CharField(null=True, blank=True)
+    past_form = models.CharField(null=True, blank=True)
+    past_participle = models.CharField(null=True, blank=True)
+    short = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return self.word
+
+
+class Videos(models.Model):
+    """Предполагаю создание словарей на основе видео"""
+    url = models.URLField()
+    name = models.CharField()
+    chanel_name = models.CharField(null=True, blank=True)
+    chanel_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dicts(models.Model):
+    """
+    is_personal: создан ли этот словарь пользователем True или является частью платформы False;
+    is_private: для созданных пользователем словарей, True - виден только пользователю создавшему словарь;
+    is_changeable: можно ли изменять словарь рядовому пользователю, кроме создавшего его;
+    """
+    is_personal = models.BooleanField(default=True)
+    video = models.ForeignKey(Videos, on_delete=models.CASCADE, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(ServiceUser, on_delete=models.DO_NOTHING)
+    change_on = models.DateTimeField(auto_now=True)
+    change_by = models.ForeignKey(ServiceUser, on_delete=models.DO_NOTHING)
+    name = models.CharField()
+    description = models.TextField(null=True, blank=True)
+    is_private = models.BooleanField(default=True)
+    is_changeable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class WordsToDicts(models.Model):
+    """
+    Связь словарей со словами.
+    serial_number: порядковый номер для словарей в которых это важно;
+    personal_translation: поле для переопределения перевода для конкретного словаря;
+    """
+    word = models.ForeignKey(Words, on_delete=models.CASCADE)
+    dictionary = models.ForeignKey(Dicts, on_delete=models.CASCADE)
+    serial_number = models.IntegerField(null=True, blank=True)
+    personal_translation = models.CharField(null=True, blank=True)
+
+
+class DictToUser(models.Model):
+    """
+    Взаимосвязь пользователей и словарей.
+    is_activ: использовать ли слова из этого словаря для изучения;
+    is_finish: изучен ли словарь полностью;
+    """
+    user = models.ForeignKey(ServiceUser, on_delete=models.CASCADE)
+    dictionary = models.ForeignKey(Dicts, on_delete=models.CASCADE)
+    is_activ = models.BooleanField(default=True)
+    is_finish = models.BooleanField(default=False)
+
+
+class UsersProgress(models.Model):
+    """
+    Прогресс пользователя в изучении слов.
+    level: уровень изучения, используется для расчета даты следующей тренировки (0 - слово только что добавлено);
+    next_training: дата следующей тренировки;
+    """
+    user = models.ForeignKey(ServiceUser, on_delete=models.CASCADE)
+    word = models.ForeignKey(Words, on_delete=models.CASCADE)
+    level = models.IntegerField()
+    next_training = models.DateTimeField()
+
+
+class Mistakes(models.Model):
+    """
+    Пока полностью не используется, функционал будет добавлен позднее.
+    Предполагается сохранять все ошибки, чтобы находить наиболее сложные слова и
+    наиболее частые неправильные ассоциации слов и подкидывать их чаще.
+    """
+    word = models.ForeignKey(Words, on_delete=models.CASCADE)
+    user = models.ForeignKey(ServiceUser, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Words, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
